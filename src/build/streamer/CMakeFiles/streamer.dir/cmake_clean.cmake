ec931842fc1bd7f5979ba5cb8c465da6b5c6a7cd
incarsaron <Bryce>
2024-04-06 11:51:30 -0400

    File structure update

---
+++ src/build/streamer/CMakeFiles/streamer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/streamer.dir/src/streamer.cpp.o"
+  "CMakeFiles/streamer.dir/src/streamer.cpp.o.d"
+  "libstreamer.pdb"
+  "libstreamer.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/streamer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/build/streamer/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /DroneWorkspace/HardwareManagers/streamer
+# Build directory: /DroneWorkspace/build/streamer
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
