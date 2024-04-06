a8dc5741c1333bbadea46ad7175f3414cfe749d7
incarsaron <Bryce>
2024-04-06 11:52:05 -0400

    File structure update

--- src/build/streamer/CMakeFiles/streamer.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for streamer.
-# This may be replaced when dependencies are built.
--- src/build/streamer/CMakeFiles/streamer_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/streamer_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/streamer_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
